84a4afc442b3f2803a4d52d807cc3749cb4048e0
Kenny Macheka <knm512@yahoo.co.uk>
2021-07-04 14:18:39 +0100

    incoming webhook

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,3 +1,4 @@
+import requests
 from datetime import datetime
 from flask import Flask, render_template
 from . import app
@@ -26,7 +27,29 @@ def hello_there(name = None):
         name=name,
         date=datetime.now()
     )
+    send_post_request()
 
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+def send_post_request():
+    data = {"type" : "message",
+            "attachments": [ {
+                 "contentType":"application/vnd.microsoft.card.adaptive",
+         "contentUrl":null,
+         "content":{
+            "$schema":"http://adaptivecards.io/schemas/adaptive-card.json",
+            "type":"AdaptiveCard",
+            "version":"1.2",
+            "body":[
+                {
+                "type": "TextBlock",
+                "text": "For Samples and Templates, see [https://adaptivecards.io/samples](https://adaptivecards.io/samples)"
+                }
+            ]
+         }
+    }] }
+
+    requests.post("https://kennymacheka.webhook.office.com/webhookb2/4aefee57-dba7-4cd6-9c14-4a2ca2755c6d@8103d5db-e42e-4c9c-af0b-4cee9bd0879e/IncomingWebhook/a869f391d77643c1b2284189b19972d0/b63c907a-d913-484a-823a-5a7e8dbb43f3",
+                  data = data)
